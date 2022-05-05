1834942dfd28aad057dbb00fe1f46bd98a0a3a84
Jacob Logue <jakelogue84@gmail.com>
2022-05-05 12:46:00 -0400

    add bootstrap and add customizable users controllers

--- Gemfile
+++ Gemfile
@@ -51,6 +51,9 @@ gem "bootsnap", require: false
 # Use devise for user authentication
 gem 'devise'
 
+# Bootstrap
+gem 'bootstrap', '~> 5.1', '>= 5.1.3'
+
 group :development, :test do
   # See https://guides.rubyonrails.org/debugging_rails_applications.html#debugging-with-the-debug-gem
   gem "debug", platforms: %i[ mri mingw x64_mingw ]
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  resources :users
-  devise_for :users
+  devise_for :users, controllers: { sessions: 'users/sessions' }
   resources :events
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
   root "events#index"
+
 end
