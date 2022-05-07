6ab0f124d09e520702b406714d0ec38dca546c46
Jacob Logue <jakelogue84@gmail.com>
2022-05-07 18:30:53 -0400

    add profile nav link, title to event#show, and create event button to profile

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -67,6 +67,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.require(:event).permit(:location, :date)
+      params.require(:event).permit(:location, :date, :title)
     end
 end
