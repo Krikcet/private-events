3fe82a260e3e8a0ebc00f517085f575a61b1073d
Jacob Logue <jakelogue84@gmail.com>
2022-05-07 18:52:41 -0400

    add username validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,6 @@ class User < ApplicationRecord
   has_many :created_events, foreign_key: :creator_id, class_name: 'Event', dependent: :destroy
   has_many :event_attendings, foreign_key: :event_attendee_id, dependent: :destroy
   has_many :attended_events, through: :event_attendings, dependent: :destroy
+
+  validates :username, presence: true, uniqueness: true, allow_nil: true
 end
