ebf22f8c136086d4730d7cf974d16749f9a891de
Jacob Logue <jakelogue84@gmail.com>
2022-05-08 11:53:57 -0400

    separate past and upcoming events using class methods

--- app/models/event.rb
+++ app/models/event.rb
@@ -14,4 +14,12 @@ class Event < ApplicationRecord
   belongs_to :creator, class_name: 'User'
   has_many :event_attendings, foreign_key: :attended_event_id, dependent: :destroy
   has_many :attendees, class_name: 'User', through: :event_attendings, source: :event_attendee, dependent: :destroy
+
+  def self.past
+    where('date < ?', Date.today)
+  end
+
+  def self.upcoming
+    where('date >= ?', Date.today)
+  end
 end
